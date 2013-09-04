4d85ff7363e1840a0679b9153c1308d6843868f9
Danesh Mondegarian <daneshm90@gmail.com>
2013-09-03 19:54:36 -0700

    Privacy Guard: Reimplement with AppOps
    Revert "framework: Privacy Guard"
    
    Change-Id: I82ab85092e54c577a0817f3bedc85063c9a3054a
    
    Conflicts:
    	core/java/android/app/IActivityManager.java
    	core/java/android/database/MemoryCursor.java
    	core/java/android/provider/Settings.java
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/symbols.xml
    	services/java/com/android/server/am/ActivityManagerService.java
    
    Privacy Guard : Reimplement backed by AppOps
    
    Re-implements privacy guard to use appops.
    
    Change-Id: I2b52e8cf0d6e126fb156f83df885b5551503cd9d
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/symbols.xml
    	services/java/com/android/server/am/ActivityStack.java

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -28,6 +28,7 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityOptions;
 import android.app.AppGlobals;
+import android.app.AppOpsManager;
 import android.app.IActivityManager;
 import android.app.IThumbnailRetriever;
 import android.app.IApplicationThread;
@@ -63,6 +64,8 @@ import android.util.EventLog;
 import android.util.Log;
 import android.util.Slog;
 import android.view.Display;
+
+import com.android.internal.app.ActivityTrigger;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
@@ -1753,7 +1756,7 @@ final class ActivityStack {
                 next.app.pendingUiClean = true;
                 next.app.thread.scheduleResumeActivity(next.appToken,
                         mService.isNextTransitionForward());
-
+                
                 checkReadyForSleepLocked();
 
             } catch (Exception e) {
@@ -1823,14 +1826,8 @@ final class ActivityStack {
             return;
         }
 
-        boolean privacy = false;
-
-        try {
-            privacy = AppGlobals.getPackageManager().getPrivacyGuardSetting(
-                    next.packageName, next.userId);
-        } catch (RemoteException e) {
-            // nothing
-        }
+        boolean privacy = mService.mAppOpsService.getPrivacyGuardSettingForPackage(
+                next.app.uid, next.packageName);
 
         if (mPrivacyGuardPackageName != null && !privacy) {
             Message msg = mService.mHandler.obtainMessage(
